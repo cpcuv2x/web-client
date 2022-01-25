3893f2bb07e8bf7697a4a438e23453adbfdb6f13
techid2000 <techid2000@gmail.com>
2022-01-26 00:50:37 +0700

    Update hook useDashboard

--- src/hooks/useDashboard.ts
+++ src/hooks/useDashboard.ts
@@ -3,56 +3,22 @@ import { useEffect, useRef, useState } from 'react'
 import ReactGridLayout from 'react-grid-layout'
 import useSWR from 'swr'
 import { v4 as uuidv4 } from 'uuid'
+
+import { DashboardItem } from '../interfaces/DashboardItem'
+import { DashboardWithItems } from '../interfaces/DashboardWithItems'
+import { LayoutWithDashboardItem } from '../interfaces/LayoutWithDashboard'
+import { WebServiceError } from '../interfaces/WebServiceError'
+
+import axiosFetcher from '../utils/axiosFetcher'
 import useDeepEffect from './useDeepEffect'
-
-// FIXME: Use common fetcher
-const fetcher = async (path: string) => {
-  const response = await axios.get(path)
-  return response.data
-}
-
-interface DashboardItem {
-  _id: string
-  metadata: string
-  __v: number
-}
-
-interface DashboardItemAssociation {
-  _id: string
-  dashboardItem: DashboardItem
-  x: number
-  y: number
-  w: number
-  h: number
-  __v: number
-}
-
-interface Dashboard {
-  _id: string
-  default: boolean
-  name: string
-  items: DashboardItemAssociation[]
-  __v: number
-}
-
-// FIXME: Move to another file and make it common
-interface WebServiceError {
-  error: string
-  message: string
-  statusCode: number
-}
-
-interface CustomizedLayout extends ReactGridLayout.Layout {
-  dashboardItem: DashboardItem
-}
 
 const useDashboard = (dashboardId: string) => {
   const fetchKey = !!dashboardId ? `/api/dashboards/${dashboardId}` : null
   const { data, error, mutate } = useSWR<
-    Dashboard | null,
+    DashboardWithItems | null,
     AxiosError<WebServiceError>
-  >(fetchKey, fetcher)
-  const [layout, setLayout] = useState<CustomizedLayout[] | undefined>(
+  >(fetchKey, axiosFetcher)
+  const [layout, setLayout] = useState<LayoutWithDashboardItem[] | undefined>(
     undefined
   )
 
@@ -71,7 +37,7 @@ const useDashboard = (dashboardId: string) => {
           association.dashboardItem
         )
       )
-      const generatedLayout = data.items.map<CustomizedLayout>(
+      const generatedLayout = data.items.map<LayoutWithDashboardItem>(
         (association) => ({
           i: association._id,
           x: association.x,
@@ -87,7 +53,7 @@ const useDashboard = (dashboardId: string) => {
 
   const updateLayout = (modifiedLayout: ReactGridLayout.Layout[]) => {
     setLayout(
-      modifiedLayout.map<CustomizedLayout>((piece) => ({
+      modifiedLayout.map<LayoutWithDashboardItem>((piece) => ({
         ...piece,
         dashboardItem: associationIdToDashboardItemMapper.current.get(piece.i),
       }))
