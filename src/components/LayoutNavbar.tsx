0a309b65cdb0fd64847bec28d7c8d714f9fc5f20
KKCORR <nattanon.hu@gmail.com>
2022-01-25 15:14:14 +0700

    style navbar using bootstrap

--- src/components/LayoutNavbar.tsx
+++ src/components/LayoutNavbar.tsx
@@ -1,28 +1,37 @@
-import { Link } from 'react-router-dom'
-import LoginLogout from './LoginLogout'
-import './LayoutNavbar.scss'
+import { useLocation } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import LogoutButton from './LogoutButton'
 
 interface Props {}
 
 const LayoutNavbar = (props: Props) => {
+  const { pathname } = useLocation()
+  const menus = [
+    { label: 'Map', href: '/map' },
+    { label: 'Cars', href: '/cars' },
+    { label: 'Drivers', href: '/drivers' },
+    { label: 'Dashboards', href: '/dashboards' },
+  ]
+
   return (
-    <div className="layout-navbar clearfix">
-      <div className="layout-navbar--link-group">
-        <Link className="layout-navbar--link" to={'/map'}>
-          Map
-        </Link>
-        <Link className="layout-navbar--link" to={'/cars'}>
-          Cars
-        </Link>
-        <Link className="layout-navbar--link" to={'/drivers'}>
-          Drivers
-        </Link>
-        <Link className="layout-navbar--link" to={'/dashboards'}>
-          Dashboards
-        </Link>
-      </div>
-      <LoginLogout />
-    </div>
+    <Navbar bg="light" expand="sm">
+      <Container className="m-0 px-4" fluid>
+        <Navbar.Brand href="/dashboards">5G-V2X</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {menus.map(({ label, href }) => (
+              <Nav.Link key={label} href={href} active={href === pathname}>
+                {label}
+              </Nav.Link>
+            ))}
+          </Nav>
+          <LogoutButton />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
