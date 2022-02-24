e771060fd42c1f77bdfb39e2f78d591987a4466a
KKCORR <nattanon.hu@gmail.com>
2022-02-24 17:27:20 +0700

    implement notification button

---
+++ src/components/NotificationButton.tsx
@@ -0,0 +1,24 @@
+import { BellOutlined } from '@ant-design/icons'
+import { Avatar, Badge, Card, Dropdown } from 'antd'
+import styles from './NotificationButton.module.less'
+
+const NotificationButton = () => {
+  // TODO: render real notifications
+  return (
+    <Dropdown
+      overlay={
+        <Card style={{ width: 300, height: 100 }} title="Notification"></Card>
+      }
+      trigger={['click']}
+      placement="bottomRight"
+    >
+      <div className={styles.container}>
+        <Badge count={3}>
+          <Avatar icon={<BellOutlined />} />
+        </Badge>
+      </div>
+    </Dropdown>
+  )
+}
+
+export default NotificationButton
