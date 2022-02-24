485c8ee149dc446b70366d9f98c89f6944269ed3
KKCORR <nattanon.hu@gmail.com>
2022-02-24 17:27:00 +0700

    implement app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { Col, Image, Layout, Row, Space, Typography } from 'antd'
+import appLogo from '../assets/app_logo.png'
+import styles from './AppHeader.module.less'
+import NotificationButton from './NotificationButton'
+import UserBadge from './UserBadge'
 
-const AppHeader = () => {
-  return <div>AppHeader</div>
-}
+const { Title } = Typography
+const { Header } = Layout
+
+const AppHeader = () => (
+  <Header className={styles['app-header']}>
+    <Row align="middle" justify="space-between">
+      <Col>
+        <Space size="middle">
+          <Image src={appLogo} preview={false} width={32} />
+          <Title level={2} className={styles['app-title']}>
+            5G-V2X
+          </Title>
+        </Space>
+      </Col>
+      <Col>
+        <Space>
+          <NotificationButton />
+          <UserBadge />
+        </Space>
+      </Col>
+    </Row>
+  </Header>
+)
 
 export default AppHeader
