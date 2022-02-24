3d7f8e6dd548c06a2d7a80de8c81b9fc1e48cbdf
KKCORR <nattanon.hu@gmail.com>
2022-02-24 17:27:39 +0700

    implement user badge

---
+++ src/components/UserBadge.tsx
@@ -0,0 +1,34 @@
+import { Dropdown, Avatar, Menu, Space, Typography } from 'antd'
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons'
+import styles from './UserBadge.module.less'
+
+const UserBadge = () => {
+  // TODO: use actually user and logout
+  const user = {
+    username: 'John Doe',
+  }
+
+  const logout = () => {
+    console.log('logout!')
+  }
+
+  const menu = (
+    <Menu>
+      <Menu.Item danger>
+        <Space onClick={logout}>
+          <LogoutOutlined /> <Typography.Text>Logout</Typography.Text>
+        </Space>
+      </Menu.Item>
+    </Menu>
+  )
+  return (
+    <Dropdown overlay={menu} placement="bottomRight">
+      <Space className={styles.container}>
+        <Avatar icon={<UserOutlined />} />
+        <Typography.Text strong>{user.username}</Typography.Text>
+      </Space>
+    </Dropdown>
+  )
+}
+
+export default UserBadge
