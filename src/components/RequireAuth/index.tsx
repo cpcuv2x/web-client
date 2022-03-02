712ff9168afaa9d96df9824e2c1d644c1186f635
KKCORR <nattanon.hu@gmail.com>
2022-03-03 03:06:23 +0700

    RequireAuth: redirect to login page if not authen

---
+++ src/components/RequireAuth/index.tsx
@@ -0,0 +1,19 @@
+import { Navigate, useLocation } from 'react-router-dom'
+import useUser from '../../hooks/useUser'
+
+const RequireAuth = ({ children }: { children: JSX.Element }) => {
+  const { user, loading } = useUser()
+  const location = useLocation()
+
+  if (loading) {
+    return <div>Redirecting...</div>
+  }
+
+  if (!user) {
+    return <Navigate to="/" state={{ from: location }} replace />
+  }
+
+  return children
+}
+
+export default RequireAuth
