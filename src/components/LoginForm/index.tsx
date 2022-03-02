bb123716f215342bbc5201d8353473480827e033
KKCORR <nattanon.hu@gmail.com>
2022-03-03 02:54:58 +0700

    implement login

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,11 +1,37 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { Form, Input, Button, Typography, Image, Space } from 'antd'
+import { Form, Input, Button, Typography, Image, Space, message } from 'antd'
+import axios, { AxiosError } from 'axios'
+
 import appLogo from '../../assets/app_logo.png'
+import useUser from '../../hooks/useUser'
+import { LoginFormDto } from '../../interfaces/LoginFormDto'
+import { User } from '../../interfaces/User'
 
 const { Title } = Typography
 
 const LoginForm = () => {
   const [form] = Form.useForm()
+  const { mutate } = useUser()
+
+  const onFinish = async ({ username, password }: LoginFormDto) => {
+    try {
+      await axios.post<User>('/api/auth/login', {
+        username,
+        password,
+        role: 'ADMIN',
+      })
+      mutate()
+    } catch (error) {
+      let msg
+      if (axios.isAxiosError(error)) {
+        msg = (error as AxiosError).response?.data || 'Some thing went wrong'
+      } else {
+        msg = (error as Error).message
+      }
+      message.error(msg)
+    }
+  }
+
   return (
     <Space direction="vertical" align="center">
       <Space direction="vertical" size="middle" align="center">
@@ -16,13 +42,13 @@ const LoginForm = () => {
       <Form
         form={form}
         layout="vertical"
-        requiredMark="optional"
         style={{ width: 350 }}
+        onFinish={onFinish}
       >
         <Form.Item name="username" required>
           <Input prefix={<UserOutlined />} placeholder="Username" />
         </Form.Item>
-        <Form.Item required>
+        <Form.Item name="password" required>
           <Input
             type="password"
             prefix={<LockOutlined />}
