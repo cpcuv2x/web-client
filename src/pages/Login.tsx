0f9f465b28b3f04fe6c7ed1a3ca06a88d61ef9d8
KKCORR <nattanon.hu@gmail.com>
2022-03-03 02:54:43 +0700

    navigate to dashboard when logged in

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react'
 import { Row, Card } from 'antd'
 import { Helmet } from 'react-helmet'
+import { useNavigate } from 'react-router-dom'
 import LoginForm from '../components/LoginForm'
+import useUser from '../hooks/useUser'
 
 const LoginPage = () => {
+  const { user, loggedOut } = useUser()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (user && !loggedOut) {
+      navigate('/dashboard/overview')
+    }
+  }, [user, loggedOut])
+
   return (
     <>
       <Helmet>
