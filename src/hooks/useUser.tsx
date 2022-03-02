04aa255e214f4ee97d0476f5bb215c874c9c09c8
KKCORR <nattanon.hu@gmail.com>
2022-03-03 02:54:09 +0700

    useUser hook

---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,17 @@
+import useSWR from 'swr'
+
+const useUser = () => {
+  const { data, mutate, error } = useSWR('/api/auth/currentuser')
+
+  const loading = !data && !error
+  const loggedOut = error && error.status === 401
+
+  return {
+    loading,
+    loggedOut,
+    user: data,
+    mutate,
+  }
+}
+
+export default useUser
