8ac174badb3a3befc802895574edaeec93518f80
KKCORR <nattanon.hu@gmail.com>
2022-03-11 14:10:49 +0700

    add BigNumber in dashboard overview page

--- src/pages/dashboard/Overview.tsx
+++ src/pages/dashboard/Overview.tsx
@@ -1,4 +1,6 @@
+import { Typography, Row, Col } from "antd"
 import { Helmet } from "react-helmet"
+import BigNumber from "../../components/widgets/BigNumber"
 
 const DashboardOverviewPage = () => {
   return (
@@ -6,7 +8,38 @@ const DashboardOverviewPage = () => {
       <Helmet>
         <title>Overview - Dashboard | 5G-V2X</title>
       </Helmet>
-      Dashboard Overview Page
+      <Typography.Title>Overview</Typography.Title>
+
+      <Row gutter={[8, 8]}>
+        <Col xs={24} sm={12} md={6}>
+          <BigNumber
+            title={"Active Car(s)"}
+            helpText={"Total number of cars that are operating."}
+            value={"2 / 3"}
+          />
+        </Col>
+        <Col xs={24} sm={12} md={6}>
+          <BigNumber
+            title={"Active Driver(s)"}
+            helpText={"Total number of drivers that are driving."}
+            value={"2 / 5"}
+          />
+        </Col>
+        <Col xs={24} sm={12} md={6}>
+          <BigNumber
+            title={"Passenger(s)"}
+            helpText={"Total number of passengers in all cars."}
+            value={"8"}
+          />
+        </Col>
+        <Col xs={24} sm={12} md={6}>
+          <BigNumber
+            title={"Weekly Accident(s)"}
+            helpText={"Total number of accidents occurred in this week."}
+            value={"1"}
+          />
+        </Col>
+      </Row>
     </div>
   )
 }
