e1a0d0388e0aa7d7d2574d2c946795858a838ac5
KKCORR <nattanon.hu@gmail.com>
2022-03-11 17:27:26 +0700

    mock dashboard driver page

--- src/pages/dashboard/Driver.tsx
+++ src/pages/dashboard/Driver.tsx
@@ -1,4 +1,8 @@
+import { Typography, Row, Col } from "antd"
 import { Helmet } from "react-helmet"
+import AccidentsLogByDriver from "../../components/widgets/AccidentsLogByDriver"
+import DrowsinessLog from "../../components/widgets/DrowsinessLog"
+import WidgetCard from "../../components/widgets/WidgetCard"
 
 const DashboardDriverPage = () => {
   return (
@@ -6,7 +10,31 @@ const DashboardDriverPage = () => {
       <Helmet>
         <title>Driver - Dashboard | 5G-V2X</title>
       </Helmet>
-      Dashboard Driver Page
+      <Typography.Title>Driver (Driver-0000)</Typography.Title>
+      <Row gutter={[16, 16]}>
+        <Col span={6}>
+          <WidgetCard
+            title={"Somchai Jaidee"}
+            helpText={"Driver information."}
+            content={
+              <Row gutter={8}>
+                <Col span={8}>Gender: </Col>
+                <Col span={16}>Male</Col>
+                <Col span={8}>Registered: </Col>
+                <Col span={16}>15/02/2022</Col>
+                <Col span={8}>Status: </Col>
+                <Col span={16}>Active</Col>
+              </Row>
+            }
+          />
+        </Col>
+        <Col span={24}>
+          <DrowsinessLog driverId="Driver-0000" />
+        </Col>
+        <Col span={24}>
+          <AccidentsLogByDriver driverId="Driver-0000" />
+        </Col>
+      </Row>
     </div>
   )
 }
