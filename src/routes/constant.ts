972cdfacf40b36ca0c5903b25c4967bf25fd7d45
KKCORR <nattanon.hu@gmail.com>
2022-03-11 18:24:47 +0700

    add routes constant

---
+++ src/routes/constant.ts
@@ -0,0 +1,8 @@
+export const routes = {
+  DASHBOARD_OVERVIEW: "/dashboard/overview",
+  DASHBOARD_CAR: "/dashboard/car",
+  DASHBOARD_DRIVER: "/dashboard/driver",
+  ENTITY_CAMERA: "/entity/camera",
+  ENTITY_CAR: "/entity/car",
+  ENTITY_DRIVER: "/entity/driver",
+}
