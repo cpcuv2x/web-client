1346700fe0ae2d4bff98ba4aa68a7b36bded539e
KKCORR <nattanon.hu@gmail.com>
2022-03-11 15:57:32 +0700

    implement mock widgets/PassengersChart

---
+++ src/components/widgets/PassengersChart/index.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import Chart from "react-apexcharts"
+import { ApexOptions } from "apexcharts"
+
+interface Props {
+  carId: string
+}
+
+const PassengersChart: React.FC<Props> = ({ carId }) => {
+  const series: ApexOptions["series"] = [
+    {
+      name: "Passengers",
+      data: [19, 22, 20, 26],
+    },
+  ]
+  const options: ApexOptions = {
+    xaxis: {
+      categories: ["2022-03-01", "2022-03-02", "2022-03-03", "2022-03-04"],
+    },
+    grid: {
+      show: false,
+    },
+    theme: {
+      mode: "dark",
+    },
+    chart: {
+      id: `passengers-${carId}`,
+      toolbar: {
+        show: false,
+      },
+    },
+  }
+  return <Chart series={series} options={options} width="100%" height={200} />
+}
+
+export default PassengersChart
