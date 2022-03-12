270668949a2661211c11c052bb4049305589353a
KKCORR <nattanon.hu@gmail.com>
2022-03-12 17:51:56 +0700

    upload image field in create car form

--- src/components/CreateCarForm/index.tsx
+++ src/components/CreateCarForm/index.tsx
@@ -1,13 +1,43 @@
-import { Button, Card, Form, Input, Modal, Space } from "antd"
+import { UploadOutlined } from "@ant-design/icons"
+import { Button, Card, Form, Input, message, Modal, Space, Upload } from "antd"
+import axios from "axios"
 import React from "react"
 import { useNavigate } from "react-router-dom"
+import useCars from "../../hooks/useCars"
+
+const normFile = (e: any) => {
+  if (Array.isArray(e)) {
+    return e
+  }
+  return e && e.fileList
+}
 
 const CreateCarForm: React.FC = () => {
   const [form] = Form.useForm()
   const navigate = useNavigate()
+  // FIXME: change offset and limit
+  const { mutate } = useCars({}, 100, 0)
 
-  const onCreate = (values: any) => {
-    console.log("values", values)
+  const onCreate = async (values: any) => {
+    const { licensePlate, model, image } = values
+    const formData = new FormData()
+    formData.append("licensePlate", licensePlate)
+    formData.append("model", model)
+    if (image && image instanceof Array && image.length) {
+      formData.append("image", image[0].originFileObj)
+    }
+
+    try {
+      await axios.post("/api/cars", formData)
+      mutate()
+      navigate("/entity/car")
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        message.error(error.response?.data)
+      } else {
+        message.error("Could not create the new car now")
+      }
+    }
   }
 
   const onCancel = () => {
@@ -27,6 +57,23 @@ const CreateCarForm: React.FC = () => {
         labelCol={{ span: 4 }}
         wrapperCol={{ span: 4 }}
       >
+        <Form.Item
+          name="image"
+          label="Image"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+        >
+          <Upload
+            name="image"
+            beforeUpload={() => false}
+            listType="picture"
+            accept="image/png, image/jpeg"
+            maxCount={1}
+          >
+            <Button icon={<UploadOutlined />}>Click to upload</Button>
+          </Upload>
+        </Form.Item>
+
         <Form.Item
           name="licensePlate"
           label="License Plate"
