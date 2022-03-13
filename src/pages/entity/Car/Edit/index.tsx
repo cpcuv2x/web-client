122088658e473f023c83616b527123ab50eb2827
KKCORR <nattanon.hu@gmail.com>
2022-03-13 16:04:35 +0700

    implement EditCarForm

--- src/pages/entity/Car/Edit/index.tsx
+++ src/pages/entity/Car/Edit/index.tsx
@@ -3,9 +3,13 @@ import { Breadcrumb, Typography } from "antd"
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useParams } from "react-router-dom"
+import EditCarForm from "../../../../components/EditCarForm"
+import useCar from "../../../../hooks/useCar"
 import { routes } from "../../../../routes/constant"
 
-const PageBreadcrumb: React.FC<{ carId: string }> = ({ carId }) => (
+const PageBreadcrumb: React.FC<{ carLicensePlate: string }> = ({
+  carLicensePlate,
+}) => (
   <Breadcrumb>
     <Breadcrumb.Item>
       <ControlOutlined />
@@ -20,26 +24,29 @@ const PageBreadcrumb: React.FC<{ carId: string }> = ({ carId }) => (
       <span>Edit</span>
     </Breadcrumb.Item>
     <Breadcrumb.Item>
-      <span>{carId}</span>
+      <span>{carLicensePlate}</span>
     </Breadcrumb.Item>
   </Breadcrumb>
 )
 
 const EntityCarEditPage: React.FC = () => {
   const { carId } = useParams()
-
   if (!carId) return <div>Loading...</div>
+
+  const { car, mutate, loading } = useCar(carId)
+  if (loading) return <div>Loading...</div>
 
   return (
     <>
       <Helmet>
-        <title>Edit Car({carId}) - Entity | 5G-V2X</title>
+        <title>Edit Car({car.licensePlate}) - Entity | 5G-V2X</title>
       </Helmet>
 
-      <PageBreadcrumb carId={carId} />
+      <PageBreadcrumb carLicensePlate={car.licensePlate} />
 
-      <Typography.Title>Edit Car: {carId}</Typography.Title>
-      <div>Editing... {carId}</div>
+      <Typography.Title>Edit Car: {car.licensePlate}</Typography.Title>
+
+      <EditCarForm initialValues={car} />
     </>
   )
 }
