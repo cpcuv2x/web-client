b498cb115f68ec65fac5c24d677113db6975acda
KKCORR <nattanon.hu@gmail.com>
2022-03-15 17:55:00 +0700

    mock passengers graph

--- src/components/widgets/PassengersChart/index.tsx
+++ src/components/widgets/PassengersChart/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Chart from "react-apexcharts"
 import { ApexOptions } from "apexcharts"
 import WidgetCard from "../WidgetCard"
@@ -9,16 +9,36 @@ interface Props {
 }
 
 const PassengersChart: React.FC<Props> = ({ carId }) => {
-  const series: ApexOptions["series"] = [
+  const series = [
     {
-      name: "Passengers",
-      data: [19, 22, 20, 26],
+      name: "passengers",
+      data: [7, 7, 7, 7, 8, 8, 9, 7],
     },
   ]
-  const options: ApexOptions = {
+  const options = {
     xaxis: {
-      categories: ["2022-03-01", "2022-03-02", "2022-03-03", "2022-03-04"],
+      categories: [
+        "18:44:50",
+        "18:45:00",
+        "18:45:10",
+        "18:45:20",
+        "18:45:30",
+        "18:45:40",
+        "18:45:50",
+        "18:46:00",
+      ],
     },
+    yaxis: [
+      {
+        labels: {
+          formatter: function (val) {
+            return val.toFixed(0)
+          },
+        },
+        min: 0,
+        max: 10,
+      },
+    ],
     grid: {
       show: false,
     },
@@ -26,22 +46,21 @@ const PassengersChart: React.FC<Props> = ({ carId }) => {
       mode: "dark",
     },
     chart: {
-      id: `passengers-${carId}`,
+      id: `passengers-charts-${carId}`,
       toolbar: {
         show: false,
       },
     },
   }
+
   return (
     <WidgetCard
-      title={"Passenger(s)"}
-      helpText={
-        "The graph between total number of passengers in this car and time."
-      }
+      title="Passenger(s)"
+      helpText="The graph between total number of passengers in this car and time."
       content={
         <div>
-          <Typography.Title level={5}>Current: 20</Typography.Title>
-          <Chart series={series} options={options} width="100%" height={200} />
+          <Typography.Title level={5}>Current: 7</Typography.Title>
+          <Chart series={series} options={options} width="100%" height={250} />
         </div>
       }
     />
