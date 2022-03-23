456a1e4b5ac350c6ea587d875a951fdf0a9a27f3
KKCORR <nattanon.hu@gmail.com>
2022-03-23 12:29:49 +0700

    mock update data

--- src/components/widgets/PassengersChart/index.tsx
+++ src/components/widgets/PassengersChart/index.tsx
@@ -2,55 +2,77 @@ import React, { useEffect, useState } from "react"
 import Chart from "react-apexcharts"
 import { ApexOptions } from "apexcharts"
 import WidgetCard from "../WidgetCard"
-import { Typography } from "antd"
+import { Button, Typography } from "antd"
 
 interface Props {
   carId: string
 }
 
+interface ChartValue {
+  series: ApexOptions["series"]
+  options: ApexOptions
+}
+
 const PassengersChart: React.FC<Props> = ({ carId }) => {
-  const series = [
-    {
-      name: "passengers",
-      data: [7, 7, 7, 7, 8, 8, 9, 7],
-    },
-  ]
-  const options = {
-    xaxis: {
-      categories: [
-        "18:44:50",
-        "18:45:00",
-        "18:45:10",
-        "18:45:20",
-        "18:45:30",
-        "18:45:40",
-        "18:45:50",
-        "18:46:00",
-      ],
-    },
-    yaxis: [
+  const [chart, setChart] = useState<ChartValue>({
+    series: [
       {
-        labels: {
-          formatter: function (val) {
-            return val.toFixed(0)
-          },
-        },
-        min: 0,
-        max: 10,
+        name: "No. of passengers",
+        data: [
+          // { x: new Date(2022, 3, 23, 10, 0, 0), y: 10 },
+          // { x: new Date(2022, 3, 23, 10, 0, 10), y: 5 },
+          // { x: new Date(2022, 3, 23, 10, 0, 20), y: 3 },
+        ],
       },
     ],
-    grid: {
-      show: false,
-    },
-    theme: {
-      mode: "dark",
-    },
-    chart: {
-      id: `passengers-charts-${carId}`,
-      toolbar: {
+    options: {
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        min: 0,
+        tickAmount: 5,
+        floating: false,
+      },
+      grid: {
         show: false,
       },
+      theme: {
+        mode: "dark",
+      },
+      chart: {
+        id: `passengers-charts-${carId}`,
+        toolbar: {
+          show: false,
+          tools: {
+            zoom: false,
+          },
+        },
+      },
     },
+  })
+
+  const appendData = () => {
+    setChart((prevChart) => {
+      const prevSeries = prevChart.series
+      const newSeries = []
+      if (prevSeries instanceof Array && prevSeries?.length > 0) {
+        const prevData = prevSeries[0]?.data
+        const prevName = prevSeries[0]?.name
+        newSeries.push({
+          name: prevName,
+          data: prevData.concat({
+            x: new Date(),
+            y: Math.floor(Math.random() * 10),
+          }),
+        })
+      }
+
+      return {
+        options: prevChart.options,
+        series: newSeries,
+      }
+    })
   }
 
   return (
@@ -60,7 +82,13 @@ const PassengersChart: React.FC<Props> = ({ carId }) => {
       content={
         <div>
           <Typography.Title level={5}>Current: 7</Typography.Title>
-          <Chart series={series} options={options} width="100%" height={250} />
+          <Button onClick={appendData}>Update</Button>
+          <Chart
+            series={chart.series}
+            options={chart.options}
+            width="100%"
+            height={250}
+          />
         </div>
       }
     />
