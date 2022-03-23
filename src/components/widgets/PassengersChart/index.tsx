8bfeccac3cb52f4d2e24e31e298466fd8e2f36a7
KKCORR <nattanon.hu@gmail.com>
2022-03-23 21:07:19 +0700

    mock chart

--- src/components/widgets/PassengersChart/index.tsx
+++ src/components/widgets/PassengersChart/index.tsx
@@ -1,77 +1,74 @@
 import React, { useEffect, useState } from "react"
 import Chart from "react-apexcharts"
 import { ApexOptions } from "apexcharts"
+import _ from "lodash"
 import WidgetCard from "../WidgetCard"
 import { Button, Typography } from "antd"
 
 interface Props {
   carId: string
+  maxPoints?: number
 }
 
-interface ChartValue {
-  series: ApexOptions["series"]
-  options: ApexOptions
+interface ChartData {
+  name: string
+  data: [number, number][]
 }
 
-const PassengersChart: React.FC<Props> = ({ carId }) => {
-  const [chart, setChart] = useState<ChartValue>({
-    series: [
-      {
-        name: "No. of passengers",
-        data: [
-          // { x: new Date(2022, 3, 23, 10, 0, 0), y: 10 },
-          // { x: new Date(2022, 3, 23, 10, 0, 10), y: 5 },
-          // { x: new Date(2022, 3, 23, 10, 0, 20), y: 3 },
-        ],
-      },
-    ],
-    options: {
-      xaxis: {
-        type: "datetime",
-      },
-      yaxis: {
-        min: 0,
-        tickAmount: 5,
-        floating: false,
-      },
-      grid: {
+const PassengersChart: React.FC<Props> = ({ carId, maxPoints = 10 }) => {
+  const [current, setCurrent] = useState(0)
+  const [series, setSeries] = useState<ChartData[]>([
+    {
+      name: "No. of passengers",
+      data: [],
+    },
+  ])
+
+  const options: ApexOptions = {
+    xaxis: {
+      type: "datetime",
+      // range: 90000,
+    },
+    yaxis: {
+      min: 0,
+      tickAmount: 5,
+      floating: false,
+    },
+    grid: {
+      show: false,
+    },
+    theme: {
+      mode: "dark",
+    },
+    chart: {
+      id: `passengers-charts-${carId}`,
+      toolbar: {
         show: false,
+        tools: {
+          zoom: false,
+        },
       },
-      theme: {
-        mode: "dark",
-      },
-      chart: {
-        id: `passengers-charts-${carId}`,
-        toolbar: {
-          show: false,
-          tools: {
-            zoom: false,
-          },
+      animations: {
+        enabled: true,
+        easing: "linear",
+        dynamicAnimation: {
+          speed: 1000,
         },
       },
     },
-  })
+  }
 
   const appendData = () => {
-    setChart((prevChart) => {
-      const prevSeries = prevChart.series
-      const newSeries = []
-      if (prevSeries instanceof Array && prevSeries?.length > 0) {
-        const prevData = prevSeries[0]?.data
-        const prevName = prevSeries[0]?.name
-        newSeries.push({
-          name: prevName,
-          data: prevData.concat({
-            x: new Date(),
-            y: Math.floor(Math.random() * 10),
-          }),
-        })
+    setSeries((prev) => {
+      const newSeries: ChartData[] = _.cloneDeep(prev)
+      if (newSeries[0].data.length >= maxPoints) {
+        newSeries[0].data.shift()
       }
-
-      return {
-        options: prevChart.options,
-        series: newSeries,
-      }
+      newSeries[0].data.push([
+        new Date().getTime(),
+        Math.floor(Math.random() * 10),
+      ])
+      return newSeries
     })
   }
 
@@ -81,14 +78,9 @@ const PassengersChart: React.FC<Props> = ({ carId }) => {
       helpText="The graph between total number of passengers in this car and time."
       content={
         <div>
-          <Typography.Title level={5}>Current: 7</Typography.Title>
+          <Typography.Title level={5}>Current: {current}</Typography.Title>
           <Button onClick={appendData}>Update</Button>
-          <Chart
-            series={chart.series}
-            options={chart.options}
-            width="100%"
-            height={250}
-          />
+          <Chart series={series} options={options} width="100%" height={250} />
         </div>
       }
     />
