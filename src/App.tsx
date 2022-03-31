42640efda751f67490cff2c90f1dbfbebf96ce95
KKCORR <nattanon.hu@gmail.com>
2022-03-31 08:25:55 +0700

    change global validation message for required rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,69 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { ConfigProvider } from "antd"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { SWRConfig } from "swr"
 import AppLayout from "./components/AppLayout"
 import RequireAuth from "./components/RequireAuth"
-import DashboardCarOverviewPage from "./pages/dashboard/Car/Overview"
 import DashboardCarPage from "./pages/dashboard/Car/Dashboard"
+import DashboardCarOverviewPage from "./pages/dashboard/Car/Overview"
 import DashboardDriverPage from "./pages/dashboard/Driver/Dashboard"
+import DashboardDriverOverviewPage from "./pages/dashboard/Driver/Overview"
 import DashboardOverviewPage from "./pages/dashboard/Overview"
 import EntityCameraPage from "./pages/entity/Camera"
-import EntityDriverPage from "./pages/entity/Driver"
+import EntityCarEditPage from "./pages/entity/Car/Edit"
+import EntityCarNewPage from "./pages/entity/Car/New"
+import EntityCarOverviewPage from "./pages/entity/Car/Overview"
+import EntityDriverNewPage from "./pages/entity/Driver/New"
 import LoginPage from "./pages/Login"
 import NotFoundPage from "./pages/NotFound"
 import axiosFetcher from "./utils/axiosFetcher"
-import DashboardDriverOverviewPage from "./pages/dashboard/Driver/Overview"
-import EntityCarEditPage from "./pages/entity/Car/Edit"
-import EntityCarOverviewPage from "./pages/entity/Car/Overview"
-import EntityCarNewPage from "./pages/entity/Car/New"
+
+const validateMessages = {
+  required: "This field is required",
+}
 
 function App() {
   return (
-    <SWRConfig value={{ fetcher: axiosFetcher }}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<LoginPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-          <Route
-            path="/"
-            element={
-              <RequireAuth>
-                <AppLayout />
-              </RequireAuth>
-            }
-          >
-            <Route path="dashboard">
-              <Route path="overview" element={<DashboardOverviewPage />} />
-              <Route path="car">
-                <Route index element={<DashboardCarOverviewPage />} />
-                <Route path=":carId" element={<DashboardCarPage />} />
+    <ConfigProvider form={{ validateMessages }}>
+      <SWRConfig value={{ fetcher: axiosFetcher }}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<LoginPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <AppLayout />
+                </RequireAuth>
+              }
+            >
+              <Route path="dashboard">
+                <Route path="overview" element={<DashboardOverviewPage />} />
+                <Route path="car">
+                  <Route index element={<DashboardCarOverviewPage />} />
+                  <Route path=":carId" element={<DashboardCarPage />} />
+                </Route>
+                <Route path="driver">
+                  <Route index element={<DashboardDriverOverviewPage />} />
+                  <Route path=":driverId" element={<DashboardDriverPage />} />
+                </Route>
               </Route>
-              <Route path="driver">
-                <Route index element={<DashboardDriverOverviewPage />} />
-                <Route path=":driverId" element={<DashboardDriverPage />} />
+              <Route path="entity">
+                <Route path="camera" element={<EntityCameraPage />} />
+                <Route path="car">
+                  <Route index element={<EntityCarOverviewPage />} />
+                  <Route path="new" element={<EntityCarNewPage />} />
+                  <Route path="edit/:carId" element={<EntityCarEditPage />} />
+                </Route>
+                <Route path="driver">
+                  <Route path="new" element={<EntityDriverNewPage />} />
+                </Route>
               </Route>
             </Route>
-            <Route path="entity">
-              <Route path="camera" element={<EntityCameraPage />} />
-              <Route path="car">
-                <Route index element={<EntityCarOverviewPage />} />
-                <Route path="new" element={<EntityCarNewPage />} />
-                <Route path="edit/:carId" element={<EntityCarEditPage />} />
-              </Route>
-              <Route path="driver" element={<EntityDriverPage />} />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </SWRConfig>
+          </Routes>
+        </BrowserRouter>
+      </SWRConfig>
+    </ConfigProvider>
   )
 }
 
