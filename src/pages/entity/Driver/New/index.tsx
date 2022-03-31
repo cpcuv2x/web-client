4adb9b4cdc282c0696372994017025fde8540975
KKCORR <nattanon.hu@gmail.com>
2022-03-31 08:25:20 +0700

    register new driver page

---
+++ src/pages/entity/Driver/New/index.tsx
@@ -0,0 +1,44 @@
+import {
+  ControlOutlined,
+  PlusCircleOutlined,
+  UserOutlined,
+} from "@ant-design/icons"
+import { Breadcrumb, Typography } from "antd"
+import { Helmet } from "react-helmet"
+import CreateDriverForm from "../../../../components/CreateDriverForm"
+import { routes } from "../../../../routes/constant"
+
+const PageBreadcrumb: React.FC = () => (
+  <Breadcrumb>
+    <Breadcrumb.Item>
+      <ControlOutlined />
+      <span>Entity</span>
+    </Breadcrumb.Item>
+    <Breadcrumb.Item href={routes.ENTITY_CAR}>
+      <UserOutlined />
+      <span>Driver</span>
+    </Breadcrumb.Item>
+    <Breadcrumb.Item>
+      <PlusCircleOutlined />
+      <span>New</span>
+    </Breadcrumb.Item>
+  </Breadcrumb>
+)
+
+const EntityDriverNewPage: React.FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>New driver - Entity | 5G-V2X</title>
+      </Helmet>
+
+      <PageBreadcrumb />
+
+      <Typography.Title>Register a new driver</Typography.Title>
+
+      <CreateDriverForm />
+    </>
+  )
+}
+
+export default EntityDriverNewPage
