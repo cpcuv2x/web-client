e820b22162f2dda6bcf63b7c183683dabf68116c
KKCORR <nattanon.hu@gmail.com>
2022-04-02 12:29:31 +0700

    add interfaces

--- src/interfaces/Car.ts
+++ src/interfaces/Car.ts
@@ -9,7 +9,22 @@ export interface Car {
   status: CarStatus
 }
 
+export interface CarsFilter {
+  licensePlate?: string
+  model?: string
+  status?: CarStatus
+  minPassengers?: number
+  maxPassengers?: number
+  limit?: number
+  offset?: number
+}
+
 export enum CarStatus {
   ACTIVE = "ACTIVE",
   INACTIVE = "INACTIVE",
+}
+
+export interface CarsResponse {
+  cars: Car[]
+  count: number
 }
