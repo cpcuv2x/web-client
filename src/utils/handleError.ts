43aa553bed4507b268bfc7bb6c66f852d3959de0
KKCORR <nattanon.hu@gmail.com>
2022-04-02 12:28:02 +0700

    add errMsg prop for customize error message

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -1,11 +1,11 @@
 import { message } from "antd"
 import axios, { AxiosError } from "axios"
 
-const handleError = (err: unknown) => {
+const handleError = (err: unknown, errMsg = "Some thing went wrong") => {
   let msg: string
   if (axios.isAxiosError(err)) {
     const axiosError = err as AxiosError<string>
-    msg = axiosError.response?.data ?? "Some thing went wrong"
+    msg = axiosError.response?.data ?? errMsg
   } else {
     msg = (err as Error).message
   }
