8c23507f7d096599900a8bb1b36ff456df263e0c
KKCORR <nattanon.hu@gmail.com>
2022-04-03 23:48:12 +0700

    temporary define limit and offset

--- src/hooks/useDrivers.tsx
+++ src/hooks/useDrivers.tsx
@@ -2,7 +2,13 @@ import useSWR from "swr"
 import { DriversFilters, DriversResponse } from "../interfaces/Driver"
 
 const useDrivers = (filters: DriversFilters = {}) => {
-  const queryString = new URLSearchParams(Object.entries(filters)).toString()
+  // FIXME: Change back to normal filters
+  const tmpFilters: DriversFilters = {
+    limit: 100,
+    offset: 0,
+    ...filters,
+  }
+  const queryString = new URLSearchParams(Object.entries(tmpFilters)).toString()
   const url = `/api/drivers?${queryString}`
 
   const { data, mutate, error } = useSWR<DriversResponse>(url)
