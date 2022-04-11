8aa967e6f242460b4f6e757b7f08c1f9ffe7d1fc
KKCORR <nattanon.hu@gmail.com>
2022-04-11 16:02:22 +0700

    add web socket url to env

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,5 +1,6 @@
 export const appConfig = {
   webServicesURL: import.meta.env.VITE_WEB_SERVICES_URL || "/",
+  webSocketURL: import.meta.env.VITE_WEB_SOCKET_URL || "/",
   googleMapAPIKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
 }
 
--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_WEB_SERVICES_URL: string
+  readonly VITE_WEB_SOCKET_URL: string
   readonly VITE_GOOGLE_MAPS_API_KEY: string
 }
 
