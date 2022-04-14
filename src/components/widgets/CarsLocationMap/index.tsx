b665b3da4c7b0c2199410e9531b9cad063fbb869
KKCORR <nattanon.hu@gmail.com>
2022-04-14 13:47:08 +0700

    temp cars locations map

--- src/components/widgets/CarsLocationMap/index.tsx
+++ src/components/widgets/CarsLocationMap/index.tsx
@@ -1,4 +1,8 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api"
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
+import { useEffect, useRef, useState } from "react"
+import { io, Socket } from "socket.io-client"
+import appConfig from "../../../configuration"
+import useCars from "../../../hooks/useCars"
 
 // Chulalongkorn university location
 const center = {
@@ -12,16 +16,54 @@ const containerStyle = {
 }
 
 const CarsLocationMap = () => {
+  const [position, setPosition] = useState<
+    Record<string, { lat: number; lng: number }>
+  >({})
+
+  const [id, setId] = useState("")
+  const socket = useRef<Socket>()
+
+  const { cars } = useCars()
+
+  useEffect(() => {
+    if (cars.length) {
+      const ids = cars.map(({ id }) => id)
+      socket.current = io("http://localhost:5000/")
+      socket.current?.on("connect", () => {
+        console.log("connected")
+        socket.current?.emit("START_STREAM_MAP_CARS", ids, (id: any) => {
+          console.log("id", id)
+          setId(id)
+          socket.current?.on(id, (x: any) => {
+            console.log(x)
+            setPosition((prev) => ({
+              ...prev,
+              [x.carId]: { lat: x.lng, lng: x.lat },
+            }))
+          })
+        })
+      })
+    }
+    return () => {
+      socket.current?.close()
+    }
+  }, [cars])
+
+  const onDisconnect = () => {
+    socket.current?.emit("STOP_STREAM", id)
+  }
+
   return (
-    <LoadScript
-      googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string}
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={16}
-      ></GoogleMap>
-    </LoadScript>
+    <>
+      {position.lat}
+      <LoadScript googleMapsApiKey={appConfig.googleMapAPIKey}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={16}>
+          {cars.map(({ id }) =>
+            position[id] ? <Marker key={id} position={position[id]} /> : <></>
+          )}
+        </GoogleMap>
+      </LoadScript>
+    </>
   )
 }
 
