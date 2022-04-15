26a213e65e46714d848ba27659d1212a07cadcc2
KKCORR <nattanon.hu@gmail.com>
2022-04-16 01:30:20 +0700

    add socket io type

---
+++ src/interfaces/socket.ts
@@ -0,0 +1,26 @@
+export type ServerToClientEvents = Record<string, (...args: any) => void>
+
+export enum SocketEventType {
+  StartStreamActiveCars = "START_STREAM_ACTIVE_CARS",
+  StartStreamActiveDrivers = "START_STREAM_ACTIVE_DRIVERS",
+  StartStreamTotalPassengers = "START_STREAM_TOTAL_PASSENGERS",
+  StartStreamTotalAccidentCount = "START_STREAM_TOTAL_ACCIDENT_COUNT",
+  StartStreamMapCars = "START_STREAM_MAP_CARS",
+  StartStreamCarInformation = "START_STREAM_CAR_INFORMATION",
+  StartStreamCarPassengers = "START_STREAM_CAR_PASSENGER",
+  StartStreamDriverInformation = "START_STREAM_DRIVER_INFORMATION",
+  StartStreamDriverECR = "START_STREAM_DRIVER_ECR",
+  StartStreamNotification = "START_STREAM_NOTIFICATION",
+  StopStream = "STOP_STREAM",
+}
+
+export interface ClientToServerEvents {
+  [SocketEventType.StartStreamMapCars]: (
+    carIds: string[],
+    callback: (sId: string) => void
+  ) => void
+  [SocketEventType.StartStreamActiveCars]: (
+    callback: (sId: string) => void
+  ) => void
+  [SocketEventType.StopStream]: (sId: string) => void
+}
