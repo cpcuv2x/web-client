49908a72cd0198a938e93a9418ab03e68cd4e34d
KKCORR <nattanon.hu@gmail.com>
2022-04-22 16:07:17 +0700

    add interfaces related with camera

---
+++ src/interfaces/Camera.ts
@@ -0,0 +1,43 @@
+export interface Camera {
+  name: string
+  description: string
+  streamUrl: string
+  role: CameraRole
+  status: CameraStatus
+  carId: string
+}
+
+export enum CameraRole {
+  DRIVER = "DRIVER",
+  DOOR = "DOOR",
+  SEATS_FRONT = "SEATS_FRONT",
+  SEATS_BACK = "SEATS_BACK",
+}
+
+export enum CameraStatus {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+}
+
+export interface CamerasResponse {
+  cameras: Camera[]
+  count: number
+}
+
+export interface CamerasFilters {
+  name?: string
+  description?: string
+  streamUrl?: string
+  carId?: string
+  limit?: number
+  offset?: number
+  orderBy?: string
+  orderDir?: OrderDir
+}
+
+export type CamerasFilter = keyof CamerasFilters
+
+export enum OrderDir {
+  ASC = "asc",
+  DESC = "desc",
+}
