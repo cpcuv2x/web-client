37ff51455b82d86dddafba756408057a9373831b
KKCORR <nattanon.hu@gmail.com>
2022-04-26 21:56:53 +0700

    add field labels for driver

---
+++ src/constants/Driver.ts
@@ -0,0 +1,21 @@
+import { DriverGender } from "../interfaces/Driver"
+
+export const fieldLabel = {
+  firstNameTH: "First Name (Thai)",
+  lastNameTH: "Last Name (Thai)",
+  firstNameEN: "First Name (Eng)",
+  lastNameEN: "Last Name (Eng)",
+  gender: "Gender",
+  birthDate: "Birth Date",
+  nationalId: "National ID",
+  carDrivingLicenseId: "Car Driving License No.",
+  image: "Image",
+  username: "Username",
+  password: "Password",
+}
+
+export const driverGenderLabel = {
+  [DriverGender.MALE]: "Male",
+  [DriverGender.FEMALE]: "Female",
+  [DriverGender.NOT_SPECIFIED]: "Not Specified",
+}
