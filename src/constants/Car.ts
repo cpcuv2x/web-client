ec48ecd6727ae3877def28753885c8f6b356f1f2
KKCORR <nattanon.hu@gmail.com>
2022-04-26 15:21:19 +0700

    add more field label

--- src/constants/Car.ts
+++ src/constants/Car.ts
@@ -1,6 +1,10 @@
 export const fieldLabel = {
+  id: "ID",
   image: "Image",
   licensePlate: "License Plate",
   model: "Model",
   cameras: "Cameras",
+  passengers: "Passenger (s)",
+  status: "Status",
+  Driver: "Driver",
 }
