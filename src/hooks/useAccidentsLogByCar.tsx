83e403104a869e7417da8a5f1e12bb41afb324aa
KKCORR <nattanon.hu@gmail.com>
2022-04-27 16:24:52 +0700

    improve filter

--- src/hooks/useAccidentsLogByCar.tsx
+++ src/hooks/useAccidentsLogByCar.tsx
@@ -1,13 +1,24 @@
 import useSWR from "swr"
-import { CarAccident } from "../interfaces/Car"
+import { AccidentLogByCar } from "../interfaces/Car"
+
+interface Filter {
+  startTime?: string
+  endTime?: string
+}
 
 const useAccidentsLogByCar = (
   carId: string,
   startTime: string,
   endTime: string
 ) => {
-  const { data, mutate, error } = useSWR<CarAccident[]>(
-    `/api/cars/${carId}/accidents?startTime=${startTime}&endTime=${endTime}`
+  const filter: Filter = {}
+  if (startTime) filter["startTime"] = startTime
+  if (endTime) filter["endTime"] = endTime
+
+  const queryString = new URLSearchParams(Object.entries(filter)).toString()
+
+  const { data, mutate, error } = useSWR<AccidentLogByCar[]>(
+    `/api/cars/${carId}/accidents?${queryString}`
   )
 
   const loading = !data && !error
