ea7eac6c3007cc4baa2031e840b2fef2bee90aef
KKCORR <nattanon.hu@gmail.com>
2022-05-04 16:33:00 +0700

    change logic

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,6 +1,6 @@
 import { notificationTitle } from "../constants/Notification"
 import { NotificationType } from "../interfaces/Notification"
 
-export function getNotificationTitle(notiType: NotificationType) {
-  return notificationTitle[notiType] ?? "Notification"
+export function getNotificationTitle(notiType?: NotificationType) {
+  return notiType ? notificationTitle[notiType] : "Notification"
 }
