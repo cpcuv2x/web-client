089ca6a2af77ae6777b922abad563771de581525
Watwe <anon.ongsakul2000@gmail.com>
2022-06-16 11:05:38 +0700

    add some notice

--- src/pages/dashboard/Heartbeat/Overview/index.tsx
+++ src/pages/dashboard/Heartbeat/Overview/index.tsx
@@ -8,55 +8,55 @@ import HeartbeatTableComponent from "../../../../components/StatusDashboard/Hear
 
 const DashboardHeartbeatOverviewPage: React.FC = () => {
 
-
-    /*Mock up data part*/  
-    var mockUpData : any[] = [];
-    const trueOrFalse : Status[] = [Status.ACTIVE, Status.INACTIVE];
-    const binaryRandom = () => {
-        return trueOrFalse[Math.floor(Math.random() * trueOrFalse.length)]
-    }
-
-    for(var i=0; i<30; i++){
-        const data : HeartbeatTableElement = {
-            "id":"AB"+i.toString(),
-            "carStatus": binaryRandom(),
-            "fontCamStatus": binaryRandom(),
-            "backCamStatus": binaryRandom(),
-            "doorCamStatus": binaryRandom(),
-            "driverCamStatus": binaryRandom(),
-            "drowsinessModuleStatus": binaryRandom(),
-            "accidentModuleStatus": binaryRandom(),
-        }
-        mockUpData.push(data)
-    }
-    /**/
-  
-    return (
-      <>
-        <Helmet>
-          <title>Heartbeat - Dashboard | 5G-V2X</title>
-        </Helmet>
-  
-        <PageBreadcrumb
-          items={[
-            {
-              label: "Dashboard",
-              icon: <AreaChartOutlined />,
-              href: routes.DASHBOARD_OVERVIEW,
-            },
-            {
-              label: "Heartbeat",
-              icon: <HeartOutlined />,
-              href: routes.DASHBOARD_HEARTBEAT,
-            },
-          ]}
-        />
-
-        <Typography.Title>Heartbeat of vehicles and devices Dashboard</Typography.Title>
-        <HeartbeatTableComponent data = {mockUpData}/>
-      </>
-    )
+  // Do not forget for loading and notfound
+  /*Mock up data part*/  
+  var mockUpData : any[] = [];
+  const trueOrFalse : Status[] = [Status.ACTIVE, Status.INACTIVE];
+  const binaryRandom = () => {
+      return trueOrFalse[Math.floor(Math.random() * trueOrFalse.length)]
   }
+
+  for(var i=0; i<30; i++){
+      const data : HeartbeatTableElement = {
+          "id":"AB"+i.toString(),
+          "carStatus": binaryRandom(),
+          "fontCamStatus": binaryRandom(),
+          "backCamStatus": binaryRandom(),
+          "doorCamStatus": binaryRandom(),
+          "driverCamStatus": binaryRandom(),
+          "drowsinessModuleStatus": binaryRandom(),
+          "accidentModuleStatus": binaryRandom(),
+      }
+      mockUpData.push(data)
+  }
+  /**/
+
+  return (
+    <>
+      <Helmet>
+        <title>Heartbeat - Dashboard | 5G-V2X</title>
+      </Helmet>
+
+      <PageBreadcrumb
+        items={[
+          {
+            label: "Dashboard",
+            icon: <AreaChartOutlined />,
+            href: routes.DASHBOARD_OVERVIEW,
+          },
+          {
+            label: "Heartbeat",
+            icon: <HeartOutlined />,
+            href: routes.DASHBOARD_HEARTBEAT,
+          },
+        ]}
+      />
+
+      <Typography.Title>Heartbeat of vehicles and devices Dashboard</Typography.Title>
+      <HeartbeatTableComponent data = {mockUpData}/>
+    </>
+  )
+}
   
-  export default DashboardHeartbeatOverviewPage
+export default DashboardHeartbeatOverviewPage
   
