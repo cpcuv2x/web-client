39a409cadb1c6238ad215840891f20c8113eba57
Watwe <anon.ongsakul2000@gmail.com>
2022-06-21 16:32:45 +0700

    edit some console.log

--- src/components/StatusDashboard/StatusCircle/index.tsx
+++ src/components/StatusDashboard/StatusCircle/index.tsx
@@ -3,7 +3,6 @@ import React, {useState, useEffect} from "react"
 import { Status } from "../../../interfaces/Status";
 
 const StatusCircle: React.FC<{status:Status|undefined}> = ({status}) =>{
-    console.log(status);
     const [color, setColor] = useState("red");
     useEffect(() => {
         if(status!==undefined) setColor(status===Status.ACTIVE ? "green": "red");
